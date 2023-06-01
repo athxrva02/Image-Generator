9964950706a4f01973d72f66878cf7dbc2a6e487
Atharva Dagaonkar <dagaonkaratharva@gmail.com>
2023-06-01 13:28:09 +0530

    Add server side

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect';
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.get('/', async (req, res) => {
+    res.send('Hello from DALL-E'); 
+})
+
+
+const startServer = async() => {
+
+    try{
+        connectDB(process.env.MONGO_URL);
+        app.listen(8080, () => console.log('Server is listening on port 8080'));
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+startServer();
